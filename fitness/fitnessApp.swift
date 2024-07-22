067dd97e206066410bd35fe94129c24c54c167b7
Chieh <iloveberit@gmail.com>
2024-07-22 23:18:29 +0800

    Initial Commit

---
+++ fitness/fitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessApp.swift
+//  fitness
+//
+//  Created by Chieh on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct fitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
